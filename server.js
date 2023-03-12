4d195b77d633d6a1c2e272a1790a4988c1e59066
Raquib Saabit <raqdeku@outlook.com>
2023-03-12 09:10:15 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const app = require("express")();
 
+// Basic Home Route
 app.get("/", (req, res) => {
   res.send("<h3>Hello There!</h3>");
 });
 
+// Route to recieve SSE
 app.get("/results", (req, res) => {
   res.setHeader("Content-Type", "text/event-stream");
   send(res);
 });
 
+// Content to send in interval of 1s
 const send = (res) => {
   let i = 0;
   setInterval(() => {
